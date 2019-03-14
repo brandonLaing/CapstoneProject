d9c27ddc02766657eda4ddc3f7b7395a750c48a2
Brandon Laing <blaing2@stumail.jccc.edu>
2019-03-14 14:08:18 -0500

    adding no platform zone

---
+++ Assets/Scripts/NoCubeZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoCubeZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerPlatformGun>().enabled = false;
+            other.GetComponent<PlayerPlatformGun>().CurrentGunState = GunState.Standby;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            other.GetComponent<PlayerPlatformGun>().enabled = true;
+    }
+}
