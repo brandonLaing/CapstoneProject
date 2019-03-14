928f459a491b6c399bade0adb28ac54bfd079bd0
Brandon Laing <blaing2@stumail.jccc.edu>
2019-03-14 14:18:46 -0500

    fuck this

---
+++ Assets/Scripts/PlayerController/PlayerInputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    public event Action OnJump = delegate { };
+    public event Action OnGunTriggered = delegate { };
+    public 
+    public event Action OnMove = delegate { };
+
+}
