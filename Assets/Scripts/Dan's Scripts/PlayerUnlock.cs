25d8562326d400d89e346a420f0e02ccf9ede4b2
Dan Towle <ddynomite1@gmail.com>
2019-03-21 21:28:35 -0500

    built the system for unlocking platforms

---
+++ Assets/Scripts/Dan's Scripts/PlayerUnlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnlock : MonoBehaviour
+{
+    public GameObject newPlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Instantiate(newPlayer, other.transform.position, other.transform.rotation);
+            Destroy(other.transform.parent.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
