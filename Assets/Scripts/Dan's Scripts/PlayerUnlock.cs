11e7ecd45e51cbee7f53d7cc90e63260faeaf9dd
svc-student <svc-student@student.directory.jccc>
2019-04-10 14:49:38 -0500

    Updated player unlock

--- Assets/Scripts/Dan's Scripts/PlayerUnlock.cs
+++ Assets/Scripts/Dan's Scripts/PlayerUnlock.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerUnlock : MonoBehaviour
 {
-    public GameObject newPlayer;
+  public ProjectionType unlockType;
+  public bool destroyOnEnter;
+  public bool unlockState;
+  public bool changesPrefab;
+  public GameObject newPrefab;
 
-    private void OnTriggerEnter(Collider other)
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.tag == "Player")
     {
-        if (other.tag == "Player")
-        {
-            Instantiate(newPlayer, other.transform.position, other.transform.rotation);
-            Destroy(other.transform.parent.gameObject);
-            Destroy(this.gameObject);
-        }
+      PlayerGunPlatformSelector selector = other.GetComponent<PlayerGunPlatformSelector>();
+      switch (unlockType)
+      {
+        case ProjectionType.StaticPlatform:
+          selector.staticAvailable = unlockState;
+          if (changesPrefab)
+            selector.staticPrefab = newPrefab;
+          break;
+
+        case ProjectionType.MovingPlatform:
+          selector.movingAvailable = unlockState;
+          if (changesPrefab)
+            selector.movingPrefab = newPrefab;
+          break;
+
+        case ProjectionType.SpeedPlatform:
+          selector.speedAvailable = unlockState;
+          if (changesPrefab)
+            selector.speedPrefab = newPrefab;
+          break;
+
+        case ProjectionType.BouncePlatform:
+          selector.bounceAvailable = unlockState;
+          if (changesPrefab)
+            selector.bounceAvailable = newPrefab;
+          break;
+      }
+
+      if (destroyOnEnter)
+        Destroy(this.gameObject);
     }
+  }
 }
