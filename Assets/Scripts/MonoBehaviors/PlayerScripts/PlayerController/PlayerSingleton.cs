5d95d5679e0313b0920a12f9fed9eba7d34b36d0
svc-student <svc-student@student.directory.jccc>
2019-05-06 12:43:43 -0500

    Player can now be set in any scene and wont mess with transition

---
+++ Assets/Scripts/MonoBehaviors/PlayerScripts/PlayerController/PlayerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleton : MonoBehaviour
+{
+  private static PlayerSingleton main;
+  [SerializeField]
+  private GameObject[] playerObjects;
+
+  private void Awake()
+  {
+    if (main == null)
+    {
+      main = this;
+      for (int i = 0; i < playerObjects.Length; i++)
+        playerObjects[i].SetActive(true);
+    }
+    else
+    {
+      Destroy(this.gameObject);
+    }
+  }
+}
